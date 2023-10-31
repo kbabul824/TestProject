64fdc2e4a515c0c2df54e77a6486677dd1d5f94e
kbabul824 <kbabul824@gmail.com>
2023-10-31 19:37:51 +0530

    made chanhes

--- src/test/java/practise/Base.java
+++ src/test/java/practise/Base.java
@@ -10,37 +10,27 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class Base {
-	public static WebDriver driver=null;
+	WebDriver driver;
 	ChromeOptions options;
 	@BeforeClass
 	public void setUp() {
-		options = new ChromeOptions();
-		
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--window-size=1024*768");
-		options.addArguments("start-maximized");
-		options.setHeadless(true);
-		
-		if(driver==null)
 		if(System.getProperty("os.name").contains("Window")){
 			System.setProperty("webdriver.chrome.driver",
 					System.getProperty("user.dir")+"\\chromedriver.exe");
-			driver = new ChromeDriver();
-			 
+			 options = new ChromeOptions();
 		}else {
 			System.setProperty("webdriver.chrome.driver",
 					System.getProperty("user.dir")+"/src/test/resources/driver/chromedriver");
-			driver = new ChromeDriver(options);
-			
+			options = new ChromeOptions();
 			
 		}
 		//options.addArguments("--disable-dev-shm-usage");
 		//options.addArguments("--window-size=1024*768");
 		//options.addArguments("start-maximized");
-		
-//		options.addArguments("--disable-gpu");
-//		options.addArguments("--no-sandbox");
-		
+		options.setHeadless(true);
+		options.addArguments("--disable-gpu");
+		options.addArguments("--no-sandbox");
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
